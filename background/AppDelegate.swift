f2f7a596e263d28f0ada1f9aead810c765b19951
Renan Toniolo Rocha <renantoniolo@gmail.com>
2021-09-22 08:43:07 -0300

    frist commit - background fecth swift

--- background/AppDelegate.swift
+++ background/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -30,6 +30,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        let randomInt = Int.random(in: 1..<50)
+        print("performFetchWithCompletionHandler result: \(randomInt)")
+        completionHandler(.newData)
+        
+       }
 
 
 }
